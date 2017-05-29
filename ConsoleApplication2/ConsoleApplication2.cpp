620f47ad9795941cd12beaa31f37f9e102e16f91
Aliang-SEU <huangzhiliang555@vip.qq.com>
2017-05-29 09:19:02 +0800

    mslde

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -9,7 +9,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	cv::Mat X = cv::imread("1.bmp",0);
 	cv::Mat Y = MSLDE::lightProcessing(X);
 	cv::normalize(Y, Y, 1, 0, cv::NORM_MINMAX);
-	cv::imshow("1", Y);
+	cv::imshow("原图片", X);
+	cv::moveWindow("原图片", 300, 200);
+	cv::imshow("处理之后的图片", Y);
+	cv::moveWindow("处理之后的图片", 500, 200);
 	cv::waitKey(0);
 	return 0;
 }
